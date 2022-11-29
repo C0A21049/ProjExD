3ba521afbf50107045737c59bae154a409f1f558
Soma <c0a210499d@edu.teu.ac.jp>
2022-11-29 17:00:43 +0900

    機能追加終わり

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,30 +1,49 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-
-def button_click(event):
-    btn = event.widget
-    txt = btn["text"]
-    
-    if txt == "=":
-        siki = entry.get()
-        res = eval(siki)
-        entry.delete(0, tk.END)
-        entry.insert(tk.END, res)
-    else: 
-        entry.insert(tk.END, txt)
+next = True
+r, c = 1, 0
+result = []
+res = 0
 
 
 root = tk.Tk()
 root.title("calc.py")
-root.geometry("400x500")
+root.geometry("374x700")
 
 
 entry = tk.Entry(justify = "right", width = 10, font = ("", 40))
 entry.grid(row = 0, column = 0, columnspan = 6)
 
 
-r, c = 1, 0
+def history():
+    rh = r
+    for res in result:
+        label = tk.Label(root, text=f"%d回目:{res}"%(rh-r+1), font=("", 15))
+        label.grid(row=rh)
+        rh += 1
+
+
+def button_click(event):
+    global next, res
+
+    btn = event.widget
+    txt = btn["text"]
+    
+    if txt == "=":
+        siki = entry.get()
+        try:
+            res = eval(siki)
+        except:
+            tkm.showwarning("警告", "計算できません\n式を入れなおしてください")
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, res)
+        result.append(res)
+        history()
+    else: 
+        entry.insert(tk.END, txt)
+
+
 operators = ["+", "-", "*", "/", "=", "."]
 for num in range(9, -3, -1):
     ope = operators[r-1]
@@ -35,7 +54,7 @@ for num in range(9, -3, -1):
         button = tk.Button(root, text=f"{operators[num]}", width=4, height=2, font=("", 30))
         button.grid(row=r, column=c)
     elif num == -1:
-        button = tk.Button(root, text=f"{operators[-1]}", width=4, height=2, font=("", 30))
+        button = tk.Button(root, text=f"{operators[num]}", width=4, height=2, font=("", 30))
         button.grid(row=r, column=c)
     else:
         button = tk.Button(root, text="", width=4, height=2, font=("", 30))
