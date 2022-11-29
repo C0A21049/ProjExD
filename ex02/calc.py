8afa16a81d902b85ad6b3b78382faa39fcffc024
Soma <c0a210499d@edu.teu.ac.jp>
2022-11-29 14:50:59 +0900

    練習2-5

--- ex02/calc.py
+++ ex02/calc.py
@@ -16,19 +16,26 @@ entry = tk.Entry(justify = "right", width = 10, font = ("", 40))
 entry.grid(row = 0, column = 0, columnspan = 3)
 
 #2
-button = [10]
-for i in range(10):
-    button = tk.Button(root,
-                          font = ("", 30),
-                          text = i)
-    if i%3 == 0:
-        button.grid(row = 13-(i+(i//3)), column = 0)
-    elif i%3 == 2:
-        button.grid(row = 9-(4*(i//3)), column = 1)
-    elif i%3 == 1:
-        button.grid(row = 9-(4*(i//3)), column = 2)
+r, c = 1, 0
+for num in range(9, -1, -1):
+    button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
+    button.grid(row=r, column=c)
+    c += 1
+    if c%3 == 0:
+        r += 1
+        c = 0
     
     #3
     button.bind("<1>", button_click)
+
+#5
+operators = ["+", "="]
+for ope in operators:
+    button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("", 30))
+    button.grid(row=r, column=c)
+    c += 1
+    if c%3 == 0:
+        r += 1
+        c = 0
 
 root.mainloop()
