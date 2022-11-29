50f4ac9a98ca5641b55c683cf589b38f74ffbb19
Soma <c0a210499d@edu.teu.ac.jp>
2022-11-29 14:37:23 +0900

    練習2-4

--- ex02/calc.py
+++ ex02/calc.py
@@ -11,6 +11,10 @@ root = tk.Tk()
 root.title("calc.py")
 root.geometry("300x500")
 
+#4
+entry = tk.Entry(justify = "right", width = 10, font = ("", 40))
+entry.grid(row = 0, column = 0, columnspan = 3)
+
 #2
 button = [10]
 for i in range(10):
@@ -18,11 +22,11 @@ for i in range(10):
                           font = ("", 30),
                           text = i)
     if i%3 == 0:
-        button.grid(row = 12-(i+(i//3)), column = 0)
+        button.grid(row = 13-(i+(i//3)), column = 0)
     elif i%3 == 2:
-        button.grid(row = 8-(4*(i//3)), column = 2)
+        button.grid(row = 9-(4*(i//3)), column = 1)
     elif i%3 == 1:
-        button.grid(row = 8-(4*(i//3)), column = 4)
+        button.grid(row = 9-(4*(i//3)), column = 2)
     
     #3
     button.bind("<1>", button_click)
