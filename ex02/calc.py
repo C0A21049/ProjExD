865e81345ac904a1e3f19be5d43af54e629a71c1
Soma <c0a210499d@edu.teu.ac.jp>
2022-11-29 17:31:00 +0900

    コメント作成 #3

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,59 +1,66 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-next = True
+
+#ボタンの配置の初期位置
 r, c = 1, 0
+#結果の保存場所
 result = []
 res = 0
 
 
+#ウィンドウサイズ
 root = tk.Tk()
 root.title("calc.py")
 root.geometry("374x700")
 
 
+#計算結果表示場所
 entry = tk.Entry(justify = "right", width = 10, font = ("", 40))
 entry.grid(row = 0, column = 0, columnspan = 6)
 
 
+#計算履歴の表示
 def history():
-    rh = r
+    rh = r#result history の表示位置
     for res in result:
         label = tk.Label(root, text=f"%d回目:{res}"%(rh-r+1), font=("", 15))
         label.grid(row=rh)
         rh += 1
 
 
+#ボタンをクリックしたときの挙動
 def button_click(event):
     global next, res
 
     btn = event.widget
     txt = btn["text"]
     
-    if txt == "=":
+    if txt == "=":#計算結果を求める
         siki = entry.get()
         try:
             res = eval(siki)
-        except:
+        except:#式が正しい型式でなかった時
             tkm.showwarning("警告", "計算できません\n式を入れなおしてください")
         entry.delete(0, tk.END)
         entry.insert(tk.END, res)
-        result.append(res)
+        result.append(res)#計算結果の保存
         history()
     else: 
         entry.insert(tk.END, txt)
 
 
+#ボタンの表示
 operators = ["+", "-", "*", "/", "=", "."]
 for num in range(9, -3, -1):
     ope = operators[r-1]
-    if num >= 0:
+    if num >= 0:#数字の表示
         button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
         button.grid(row=r, column=c)
-    elif num == -2:
+    elif num == -2:#=の表示
         button = tk.Button(root, text=f"{operators[num]}", width=4, height=2, font=("", 30))
         button.grid(row=r, column=c)
-    elif num == -1:
+    elif num == -1:#.の表示
         button = tk.Button(root, text=f"{operators[num]}", width=4, height=2, font=("", 30))
         button.grid(row=r, column=c)
     else:
@@ -61,7 +68,7 @@ for num in range(9, -3, -1):
         button.grid(row=r, column=c)
     button.bind("<1>", button_click)
     c += 1
-    if c%3 == 0:
+    if c%3 == 0:#計算記号を表示し、次の段に移動する
         button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("", 30))
         button.grid(row=r, column=c)
         r += 1
