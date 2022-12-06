2b11fdb185dbba2ba931085b4e85d27361f5789a
Soma <c0a210499d@edu.teu.ac.jp>
2022-12-06 13:20:19 +0900

    ex3-1

---
+++ ex03/maze.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+# def count_up():
+#     global tmr
+#     label["text"] = tmr
+#     tmr = tmr+1
+#     root.after(1000,count_up)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    # label = tk.Label(root, font=("", 80))
+    # label.pack()
+
+    # tmr = 0
+    # count_up()
+    
+    root.title("迷えるこうかとん")
+    # root.geometry("1500x900")
+    # canvas = tk.Canvas(
+    #     root,
+    #     width=1500,
+    #     height=900,
+    #     bg="black")
+    # canvas.pack()
+    
+    root.mainloop()
